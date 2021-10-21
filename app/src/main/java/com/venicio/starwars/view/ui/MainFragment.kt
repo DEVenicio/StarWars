f07d94ed09656cf1eb7581bb860222cfa20c4389
DEVenicio <engineer.venicio@outlook.com>
2021-10-21 14:23:15 -0300

    feat: create layout screen main

--- app/src/main/java/com/venicio/starwars/view/ui/MainFragment.kt
+++ app/src/main/java/com/venicio/starwars/view/ui/MainFragment.kt
@@ -1,10 +1,11 @@
-package com.venicio.starwars
+package com.venicio.starwars.view.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.venicio.starwars.R
 
 
 class MainFragment : Fragment() {
